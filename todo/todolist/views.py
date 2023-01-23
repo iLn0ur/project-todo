458c0902caf09e4f8f9370a9d1dddad8069828d4
iLn0ur <you@example.com>
2023-01-23 13:34:00 +0300

    todolist create

--- todo/todolist/views.py
+++ todo/todolist/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = ProjectToDo.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class ToDoModelViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
